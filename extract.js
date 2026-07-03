2d846e003ac2abf2c26ff297a952ea14f61b1f25
MH <2918436515@qq.com>
2026-07-03 23:02:28 +0800

    Delete extract.js

--- extract.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('C:\\Users\\guoyu\\AppData\\Roaming\\VClaw\\.openclaw\\workspace\\异物系统\\食品生产车间异物发现统计分析系统-云同步版.html', 'utf8');
-const m = html.match(/<script>([\s\S]*?)<\/script>/);
-if (m) {
-  fs.writeFileSync('C:\\Users\\guoyu\\AppData\\Local\\Temp\\check_syntax.js', m[1], 'utf8');
-  console.log('JS extracted, length:', m[1].length);
-}
